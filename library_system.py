b5e37e51a734ac4eaffad69384c90df771cbc650
Joao Vitor Araujo de Sousa <01709974133@sefaz.to.gov.br>
2025-07-08 13:06:42 -0300

    Funct to add new book

--- library_system.py
+++ library_system.py
@@ -1,10 +1,25 @@
 class Book:
-    def __init__(self, title, author, ISBN):
+    def __init__(self, ISBN, title, author):
         self.title = title
         self.author = author
         self.isbn = ISBN
         self.available = True
         self.left = self.right = None
+        
+        
+    def add_book(self, ISBN, title, author):
+        if ISBN < self.isbn:
+            if self.left == None:
+                self.left = Book(ISBN, title, author)
+                return
+            self.left.add_book(ISBN, title, author)
+            return
+        
+        if self.right == None:
+            self.right = Book(ISBN, title, author)
+            return
+        self.right.add_book(ISBN, title, author)
+
     
 
 
@@ -12,7 +27,7 @@ class Users:
     def __init__(self):
         self.user = {}
         
-    def new_user(self, name, id):
+    def new_user(self, id, name):
         self.user[id] = {
             "name": name,
             "loans": []
@@ -23,3 +38,12 @@ class Users:
 class Library:
     def __init__(self):
         self.root = None
+        
+    
+    def add_book(self, ISBN, title, author):
+        if self.root != None:
+            self.root.add_book(ISBN, title, author)
+            return print('New book cataloged!')
+        
+        self.root = Book(ISBN, title, author)
+        return print('New book cataloged!')
---
+++ library_test.py
@@ -0,0 +1,21 @@
+import library_system as ls
+
+library = ls.Library()
+
+books_data = [
+    {"ISBN": "978-0743273565", "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
+    {"ISBN": "978-0061120084", "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    {"ISBN": "978-0451524935", "title": "1984", "author": "George Orwell"},
+    {"ISBN": "978-0547928227", "title": "The Hobbit", "author": "J.R.R. Tolkien"},
+    {"ISBN": "978-0385732559", "title": "The Catcher in the Rye", "author": "J.D. Salinger"},
+    {"ISBN": "978-0345391803", "title": "The Hitchhiker's Guide to the Galaxy", "author": "Douglas Adams"},
+    {"ISBN": "978-0743273565", "title": "Pride and Prejudice", "author": "Jane Austen"}, # Teste de duplicidade
+    {"ISBN": "978-0307277747", "title": "The Da Vinci Code", "author": "Dan Brown"},
+    {"ISBN": "978-0439023528", "title": "The Hunger Games", "author": "Suzanne Collins"},
+    {"ISBN": "978-0062315811", "title": "The Alchemist", "author": "Paulo Coelho"}
+]
+
+for book in books_data:
+    library.add_book(book["ISBN"], book["title"], book["author"])
+# Falta implementear a verificação se o código já existe no sistema.
+# Fazer isso como prioridade.
