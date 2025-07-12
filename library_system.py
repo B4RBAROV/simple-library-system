9cb177e2ea6b60502d29e3daa7b5d19e847d3302
unknown <jjvitor97cpm@gmail.com>
2025-07-12 15:16:56 -0300

    Change print funct to Library

--- library_system.py
+++ library_system.py
@@ -8,47 +8,41 @@ class Book:
         
         
     def __str__(self):
-        return 'Livro: {} ({});\nBy: {}.\n'.format(self.title, self.isbn, self.author)
+        return 'ISBN: {};\nLivro: {};\nBy: {}.\n'.format(self.isbn, self.title, self.author)
         
         
     def add_book(self, isbn, title, author):
         if isbn == self.isbn:
-            print('ISBN already exists!\n')
-            return
+            return False
         
         if isbn < self.isbn:
             if self.left == None:
                 self.left = Book(isbn, title, author)
-                print('New book cataloged!\n')
-                return
+                return True
             
             return self.left.add_book(isbn, title, author)
         
         if self.right == None:
             self.right = Book(isbn, title, author)
-            print('New book cataloged!\n')
-            return
+            return True
             
         return self.right.add_book(isbn, title, author)
     
     
     def find_book(self, isbn):
         if isbn == self.isbn:
-            return print(str(self))
+            return self
         
         if isbn < self.isbn:
             if self.left == None:
-                print("Didn't find any book with ISBN = {}\n".format(isbn))
                 return
             
-            self.left.find_book(isbn)
-            return
+            return self.left.find_book(isbn)
         
         if self.right == None:
-            print("Didn't find any book with ISBN = {}\n".format(isbn))
             return
         
-        self.right.find_book(isbn)
+        return self.right.find_book(isbn)
     
 
 
@@ -59,14 +53,13 @@ class User:
         
     def new_user(self, id, name):
         if id in self.user:
-            print('User id already exists!\n')
-            return
+            return False
         
         self.user[id] = {
             "name": name,
             "loans": []
         }
-        print('User registered!\n')
+        return True
         
 
 
@@ -78,19 +71,34 @@ class Library:
     
     def add_book(self, isbn, title, author):
         if self.root != None:
-            self.root.add_book(isbn, title, author)
-            return
+            sys_return = self.root.add_book(isbn, title, author)
+            
+            if sys_return:
+             return print('New book cataloged!\n')
+         
+            return print('ISBN already exists!\n')
         
         self.root = Book(isbn, title, author)
+        print('New book cataloged!\n')
     
     
     def add_user(self, id, name):
-        self.user_manager.new_user(id, name)
+        sys_return = self.user_manager.new_user(id, name)
+        
+        if sys_return:
+            return print('User registered!\n')
+        
+        print('User id already exists!\n')
         
         
     def find_book(self, isbn):
         if self.root != None:
-            self.root.find_book(isbn)
-            return 
+            sys_return = self.root.find_book(isbn)
+            
+            if sys_return != None:
+                return print(sys_return.__str__())
+            
+            return print("Didn't find any book with ISBN = {}\n".format(isbn))
+             
         
         print("No books have been cataloged in this library yet!")
